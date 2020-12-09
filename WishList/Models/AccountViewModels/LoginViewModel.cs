6eea79858e793b812c00c035b3207eb4bc1f31b2
Harry Burgess <hburgess1990@gmail.com>
2020-12-09 21:25:51 +0000

    Create login functionality

---
+++ WishList/Models/AccountViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models.AccountViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+    }
+}
